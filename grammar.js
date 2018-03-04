a09b57841d3880e6dc7b6e5c4a7240f1e6d227d2
John Goodman <johncurtisgoodman@gmail.com>
2018-03-04 18:02:46 -0500

    Add capitalize function.

--- grammar.js
+++ grammar.js
@@ -1,19 +1,22 @@
 var commaList = (list) => {
-  var string;
-  string = '';
-  list.map((name, index) => {
-    if (list.length === 1) {
-      string = name;
-    } else if (index === list.length - 1) {
-      string += list.length === 2 ?
-      ' and ' + name :
-      ', and ' + name ;
-    } else if (index === 0) {
-      string += name;
-    } else {
-      string += ', ' + name;
-    }
-  });
-  return string;
+    var string;
+    string = '';
+    list.map((name, index) => {
+        if (list.length === 1) {
+            string = name;
+        } else if (index === list.length - 1) {
+            string += list.length === 2 ?
+            ' and ' + name :
+            ', and ' + name ;
+        } else if (index === 0) {
+            string += name;
+        } else {
+            string += ', ' + name;
+        }
+    });
+    return string;
 }
 
+var capitalize = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}

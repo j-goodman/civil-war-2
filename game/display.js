2a6d831080ace1d4eeff044d9bdf8eaf3df94f0e
John Goodman <johncurtisgoodman@gmail.com>
2018-03-05 23:15:19 -0500

    Add movement controls.

--- game/display.js
+++ game/display.js
@@ -3,16 +3,36 @@ var display = {};
 window.onload = () => {
     display.main = new Block ('block');
     display.main.addToPage();
-    display.main.text('The North American Civil War has raged for a hundred years. The continent is rocked by cannonfire. You\'re the wagon sargeant of a unit of six soldiers, your job is to manage the rations and equipment.');
+    display.main.text('The North American Civil War has raged for a hundred years. The continent is rocked by cannonfire. You\'re the wagon sargeant of a unit of five soldiers, your job is to manage the rations and equipment.');
     
     display.location = new Block ('bubble', 'LOCATION');
     display.location.update = () => {
-        var locationDescription = 'Your unit is in the ' + COPY.territoryDescriptors[game.player.unit.location.biome] + ' of ' + game.player.unit.location.name + '. On your borders are ';
+        var march;
+        var block;
+        var action;
+        var locationDescription = 'Your unit is in the ' + COPY.territoryDescriptors[game.player.unit.location.biome] + ' of ' + game.player.unit.location.name.toUpperCase() + '. On your borders are ';
         locationDescription += commaList(game.player.unit.location.borderNames) + '.';
         display.location.content = locationDescription;
         display.location.node.innerText = display.location.node.className.includes('small') ?
             display.location.title:
             display.location.content;
+        march = new Block ('bubble', 'March');
+        march.content = `What adjacent territory do you want to march your unit to?`;
+        march.childBlocks = [];
+        game.player.unit.location.borderNames.map((name) => {
+            block = new Block ('bubble', name);
+            block.content = `Our scouts aren't reporting any signs of enemy action in ${name}.`;
+            action = new Block (
+                'action',
+                `Go to ${name}.`,
+                game.player.unit.goTo.bind(game.player.unit, name),
+            );
+            block.childBlocks = []
+            block.childBlocks.push(action);
+            march.childBlocks.push(block);
+        });
+        display.location.childBlocks = [];
+        display.location.childBlocks.push(march);
     }
     
     display.troops = new Block ('bubble', 'TROOPS');
@@ -25,12 +45,13 @@ window.onload = () => {
         }).length + ' cavalry, and ' + game.player.unit.troops.filter((soldier) => {
             return soldier.division === 'artillery'
         }).length + ' artillery.'
+        display.troops.childBlocks = []
         game.player.unit.troops.map((soldier) => {
             block = new Block ('bubble', soldier.name + ', ' + capitalize(soldier.division));
             block.content = `${soldier.rank} ${soldier.name}
-            ${capitalize(soldier.division)}
+            ${capitalize(soldier.division)},
             
-            Healthy.
+            healthy.
             
             From the ${soldier.home.type} of ${soldier.home.name}.
             Speaks ${commaList(Object.keys(soldier.languages).map((lang) => {
--- game/person.js
+++ game/person.js
@@ -11,13 +11,11 @@ var Person = function (division, homeName) {
             ]
         ] = ['basic', 'proficient', 'fluent'][Math.floor(Math.random() * 3)];
     }
-    // Assign name:
-    var lang;
-    lang = pickRandom(this.home.languages);
-    this.languages[lang] = this.languages[lang] ? this.languages[lang] : 'basic';
-    if (namesByLanguage[lang]) {
-        this.name = generateName(lang);
-    } else {
-        this.name = generateName(this.home.languages[0]);
-    }
+    
+    // Name character:
+    this.name = generateName(this);
+}
+
+Person.prototype.languageList = function () {
+    return Object.keys(this.languages);
 }
